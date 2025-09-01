1f73e2e5820fa32da1b988adb138bd8a1b03bcc5
Neil Ed <trainee91.glophics@gmail.com>
2025-09-01 09:18:03 +0800

    feat: partial tailwind setup

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,26 +5,23 @@ export default function Home() {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <div className="flex items-center justify-center text-black flex-col gap-8">
-      <h1 className="text-red-500 text-3xl font-bold text-center">
-        Hello Electron + React + Tailwind! Basic Counter App!
-      </h1>
-      <div className="text-3xl font-bold">{count}</div>
-      <div className="flex flex-row gap-4">
-        <button
-          className="border border-black p-4 rounded-sm cursor-pointer"
-          onClick={() => setCount(count + 1)}
-        >
-          Increment
-        </button>
-        <button
-          className="border border-black p-4 rounded-sm cursor-pointer"
-          onClick={() => setCount(count - 1)}
-        >
-          Decrement
-        </button>
+    <section className="flex items-center justify-center text-black flex-col gap-8 w-screen h-screen">
+      <div className="flex flex-row items-center gap-4 mb-8">
+        logo
+        <h1 className="text-3xl font-bold text-center">Invoice System</h1>
       </div>
-      <Link to={"/user"}>Users Page</Link>
-    </div>
+      <Link
+        to={"/user"}
+        className="bg-[#0FB799] w-1/2 text-center p-2 rounded-lg text-white font-bold"
+      >
+        QR Mode
+      </Link>
+      <Link
+        to={"/user"}
+        className="bg-black text-white w-1/2 p-2 rounded-lg font-bold text-center"
+      >
+        Configurations
+      </Link>
+    </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    "./src/renderer/**/*.{js,ts,jsx,tsx}"
+     "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
